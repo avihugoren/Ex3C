ad101ea0770265fc63d931eca868a540568df19c
avihugoren <73278156+avihugoren@users.noreply.github.com>
2020-12-30 22:15:42 +0200

    final 1.1

--- isort.c
+++ isort.c
@@ -1,58 +1,54 @@
 #include <stdio.h>
 
 #define LEN 50
-#define LINE 256
 
 
 void shift_element(int* arr, int i){
 
-    int* m=arr+i;
+    int* ptr_element=arr+i;//pointing to  the last element that need to move
+    //for loop that runs from the last element and move each element one cell to the right
     for(int j=i-1;j>=0;j--)
     {
-        *m=*(arr+j);
-        m--;
+        *ptr_element=*(arr+j);
+        ptr_element--;
     }
 }
-insertion_sort(int* arr , int len)
+void insertion_sort(int* arr , int len)
 {
     int* pointer=arr;
-    int counter;
+    int counter;//count how many elements needs to shift
     int temp;
     for(int i=1;i<len;i++)
     {
         counter=0;
+        //for loop that insert arr[i] into his place on the sorted array from index 0 to i and how many elements need to get shifted
         for(int j=i;j>0;j--)
         {
             if(*(arr+i)<*(arr+j-1))
             {
-                pointer=arr+j-1;
-                counter++;
+                pointer=arr+j-1;//the place where arr[i] will be inserted
+                counter++;//how many elements need to get shifted
             } else
                 break;
         }
-        temp=*(arr+i);
-        shift_element(pointer,counter);
-        *(arr+i-counter)=temp;
+        temp=*(arr+i);//copy the value of arr[i]
+        shift_element(pointer,counter);//shifting the elements
+        *(arr+i-counter)=temp;//insert arr[i] to this place in the sorted array
     }
 }
 int main()
 {
-    int arr2[5]={4,3,2,1,0};
-    insertion_sort(arr2,4);
-    for (int i = 0; i <4 ; ++i) {
-        printf("%d",arr2[i]);
-
-    }
     int arr [50];
-    int x;
+    int input;
+    //for loop that gets 50 numbers from the user
     for(int i=0;i<50;i++)
     {
-        scanf("%d",&x);
-        *(arr+i)=x;
+        scanf("%d",&input);
+        *(arr+i)=input;//insert the value that taken from the user into the array
     }
 
-    insertion_sort(arr,50);
-    printf("\n");
+    insertion_sort(arr,50);//sort the array
+    //for loop that print the sorted array
     for(int i=0;i<49;i++)
     {
         printf("%d,",*(arr+i));
@@ -62,16 +58,4 @@ int main()
 }
 
 
-//     int arr[7]={8,1,0,0,3,4,2};
-//    // shift_element(arr,2);
-//     for(int i=0;i<7;i++)
-//     {
-//         printf("%d,",arr[i]);
-//     }
-//     insertion_sort(arr,7);
-//     printf("\n");
-//     for(int i=0;i<7;i++)
-//     {
-//         printf("%d,",arr[i]);
-//     }
-//     return 0;
+
--- txtfind.c
+++ txtfind.c
@@ -3,6 +3,7 @@
 #define WORD 30
 #define MAX_LINES 250
 #include "stdlib.h"
+//function that gets line from the txt and save it in s[]
 int myGetline(char s[])
 {
     char ch;
@@ -13,6 +14,7 @@ int myGetline(char s[])
        {
            s[counter]=ch;
            counter++;
+           //the options to end line
            if (ch =='\n')
                break;
            if (ch ==EOF)
@@ -22,6 +24,7 @@ int myGetline(char s[])
     }
     return counter;
 }
+//functon that gets word from the text and put it in w[]
 int getword(char w[])
 {
     int counter=0;
@@ -32,19 +35,23 @@ int getword(char w[])
             w[counter]=temp;
             counter++;
         }
+        //all the options to end word
         if(temp=='\n'||temp=='\t'||temp==' '||temp==EOF||temp=='\0')
             return counter;
     }
     return counter;
 }
+//function that receive string and check its length
 int strLegnth(char *str1)
 {
     int counter=0;
+    //while its not the end of the string
     while (str1[counter]!='\0'&&str1[counter]!= ' '&&str1[counter]!='\n'&&str1[counter]!='\t'&&str1[counter]!=EOF&&str1[counter]!='\r')
             counter++;
     return counter;
 }
-int lineLengh(char *str)
+//function that receive line and check its length
+int lineLength(char *str)
 {
     int counter =0;
     while (str[counter]!='\n'&&str[counter]!=EOF)
@@ -54,67 +61,67 @@ int lineLengh(char *str)
 int substring( char * str1, char * str2)
 {
     int counter=0;
-    int str1_length = lineLengh(str1);
+    int str1_length = lineLength(str1);
     int str2_length = strLegnth(str2);
-//    printf("str 1 length is : %d\n",str1_length);
-//    printf("str 2 length is : %d\n",str2_length);
-//    for (int i = 0; i < str1_length; ++i) {
-//        printf("%c",str1[i]);
-//    }
-//    printf("\n");
     if(str2_length > str1_length)
         return 0;
+    //for loop that runs from 0 to str1 length-str2 length and check if str2 is substring of str 1
     for (int i = 0; i <=str1_length-str2_length ; ++i)
-    {
         for (int j = 0; j <str2_length ; ++j)
-        {
+            //if the equal then counter get +1
           if(str2[j]==str1[i+j])
           {
               counter++;
+              //its mean that str1 contain str 2
               if(counter==str2_length)
                   return 1;
           }
-          else
+          else//they not equal so set counter to zero
           {
               counter=0;
               break;
           }
-        }
-    }
+
     return 0;
 }
 int similar (char *s, char *t)
 {
+    //the length is equal just check if its substring
     if(strLegnth(s) == strLegnth(t))
     {
         return substring(s,t);
     }
+    //if the difference is lengths is bigger then 1 so they cant be similar
     if(strLegnth(s)-1 != strLegnth(t))
         return 0;
     int counterRemove=0;
     int indexT=0;
     for(int i=0;i<strLegnth(s);i++)
     {
+        //if they equal so both get +1
         if(s[i] == t[indexT])
             indexT++;
+        //they not equal so makes counterRemove +1 indexT stay as is
         else{
             counterRemove++;
         }
+        //need to remove more then one char so they not equal
         if(counterRemove>1)
             return 0;
     }
     return 1;
 }
-void print_lines(char * str) {
+void print_lines(char * str)
+{
     char line[LINE];
     int x = 0;
-    for (int i = 0; i < MAX_LINES; ++i) {
-        x = myGetline(line);
-//        for (int k = 0; k <x ; ++k)
-//            printf("%c",line[k]);
+    //runs on the Max lines of the text
+    for (int i = 0; i < MAX_LINES; ++i)
+    {
+        x = myGetline(line);//copy line from the text into line
+        //if str is sub string in the line then print it
         if (substring(line, str))
         {
-//            printf("debug");
             for (int j = 0; j < x; ++j)
                 printf("%c", line[j]);
 
@@ -126,38 +133,42 @@ void print_lines(char * str) {
 void print_similar_words(char *str) {
     int x = 0;
     char word[WORD];
+    //runs on the Max lines of the text
     for (int i = 0; i < MAX_LINES; i++)
+        //runs on the maximum words a line can hold (size of line/size of word)
         for (int j = 0; j < (double)LINE/WORD; ++j)
         {
-            x = getword(word);
+            x = getword(word);//copy the word from the line into word
+            //if it similar with str then print it
             if (similar(word, str))
             {
                 for (int k = 0; k < x; ++k)
                     printf("%c", word[k]);
-                if(word[x-1]!='\n'&&x>0)
+                if(word[x-1]!='\n'&&x>0)//checks if the word didnt contain \n
                     printf("\n");
-//                    printf("\n");
             }
         }
 
 }
 
     int main() {
-        char c[WORD];
-        int x = getword(c);
-        char *temp = (char *) malloc(x);
+        char str[WORD];//to hold the str from the txt
+        int x = getword(str);//copy the string that we use in function to str from the txt
+        char *temp = (char *) malloc(x);//to hold str with no empty space after x (size of str)
         if (temp == NULL) {
             printf("Error");
             return -1;
         }
+        //copy str into temp
         for (int i = 0; i <x; i++)
-            temp[i] = c[i];
-//        temp[x] = '\0';
-        char c1[1];
+            temp[i] = str[i];
+        char c1[1];//to hold a or b
         getword(c1);
-        if (c1[0] == 'a') {
+        if (c1[0] == 'a')
+        {
             print_lines(temp);
-        } else print_similar_words(temp);
+        }
+        else print_similar_words(temp);
         free(temp);
         return 0;
     }
